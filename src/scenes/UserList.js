d389c2d9bfb3f8d8cbf91d31f39494b34d4bae6c
Diana Marcelin <marcelindiana@yahoo.com>
2022-03-01 14:46:02 -0500

    complete security

--- src/scenes/UserList.js
+++ src/scenes/UserList.js
@@ -3,7 +3,13 @@ import { useEffect, useState } from "react";
 function UserList({ token }) {
   const [userList, setUserList] = useState();
   useEffect(() => {
-    fetch("http://localhost:3001/users")
+    fetch("http://localhost:3001/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
       .then((response) => response.json())
       .then((data) => setUserList(data.users))
       .catch(alert);
